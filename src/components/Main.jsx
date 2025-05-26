139c422d816c2353f7a23dcbf94524ca400614f9
kangaroo19 <1000jjj@naver.com>
2025-05-26 22:57:58 +0900

    craco 라이브러리 사용하여 rfp 라이브러리 에러 수정
    https://stackoverflow.com/questions/70964723/webpack-5-in-create-react-app-cant-resolve-not-fully-specified-routes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,6 @@
 import AppRouter from 'routes/AppRouter'
 import Layout from './layout/Layout'
+import ReactFloatingProfile from 'react-floating-profile'
 
 export default function Main() {
   return (
@@ -8,6 +9,11 @@ export default function Main() {
       <Layout.WindowRender />
       <Layout.TaskBar />
       <AppRouter />
+      <ReactFloatingProfile
+        userName="kangaroo19" // required
+        // 💡 accessToken is not required, but it's recommended to avoid GitHub API rate limits.
+        location="top-right"
+      />
     </Layout>
   )
 }
