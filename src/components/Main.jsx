c4ebfd87c70329fa89bded7e602bce784a52e104
kangaroo19 <1000jjj@naver.com>
2025-05-29 21:40:30 +0900

    deploy error

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,21 +1,21 @@
 import AppRouter from 'routes/AppRouter'
 import Layout from './layout/Layout'
-import ReactFloatingProfile from 'react-floating-profile'
+// import ReactFloatingProfile from 'react-floating-profile'
 
 export default function Main() {
-  const githubApiKey = process.env.REACT_APP_GITHUB_API_KEY
+  // const githubApiKey = process.env.REACT_APP_GITHUB_API_KEY
   return (
     <Layout>
       <Layout.Background />
       <Layout.WindowRender />
       <Layout.TaskBar />
       <AppRouter />
-      <ReactFloatingProfile
+      {/* <ReactFloatingProfile
         userName="kangaroo19" // required
         accessToken={githubApiKey} // props로 전달
         pinnedRepoArr={['neki99', 'react-floating-profile']}
         location="top-right"
-      />
+      /> */}
     </Layout>
   )
 }
